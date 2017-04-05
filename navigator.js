a89ec5204afa44ae226942e4351f7d5af1c54d7f
Nenomaz <nazzarenomazzitello@gmail.com>
2017-04-05 11:45:02 +0200

    first release

--- navigator.js
+++ navigator.js
@@ -1,16 +1,20 @@
 var Navigator = {
 	actions: [],
 	to: function(action) {
-		action(arguments[1], arguments[2], arguments[3]);
-		arguments.shift();
-		this.actions.push({
-			action: action,
-			'arguments': arguments
-		});
+		if (typeof action != "function") {
+			console.error("Navigator.to(action): first argument isn't a function")
+		} else {
+			action();
+			this.actions.push(arguments);
+		}
 	},
 	back: function() {
-		this.actions.pop();
-		var backAction = this.actions.pop();
-		backAction();
+		if (this.actions.length < 2) {
+			console.warn("Navigator.back(): isn't possible going back");
+			return;
+		}
+		var currentAction = this.actions.pop();
+		var backAction = this.actions[this.actions.length - 1];
+		backAction[0]();
 	}
 };
